d503919fb4095869ae7923c200f3367a3dcc57f6
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-10-23 12:15:50 +0100

    Added basic physics too player

--- Cave Divers/Assets/Scripts/Player.cs
+++ Cave Divers/Assets/Scripts/Player.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    [SerializeField] float mouseSensitivity = 1000f;
+    [Header("Player Movement")]
     [SerializeField] float moveSpeed = 12f;
+    [Header("Looking around")]
+    [SerializeField] float mouseSensitivity = 1000f;
     [SerializeField] Transform cam;
+    [Header("Ground Checking")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float checkRadius = 0.01f;
+    [SerializeField] LayerMask whatIsGround;
 
     CharacterController control;
     private float xRotation = 0f;
+    private float gravity = -9.81f;
+    private Vector3 fallVelocity;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -25,12 +33,24 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
+        //Check if grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, checkRadius, whatIsGround);
+        //If grounded set fall velocity to small number 
+        if(isGrounded == true)
+        {
+            fallVelocity.y = -2f;
+        }
+        //Deal with WASD movement
         float xMov = Input.GetAxis("Horizontal");
         float yMov = Input.GetAxis("Vertical");
 
         Vector3 movement = transform.right * xMov + transform.forward * yMov;
 
         control.Move(movement * moveSpeed * Time.deltaTime);
+
+        //Falling
+        fallVelocity.y += gravity * Time.deltaTime;
+        control.Move(fallVelocity * Time.deltaTime);
     }
 
     private void PlayerLook()
@@ -42,5 +62,10 @@ public class Player : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
         cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
     }
 }
