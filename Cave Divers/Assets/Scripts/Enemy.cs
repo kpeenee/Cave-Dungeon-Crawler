63730f6adafc7bcf3a9a47d00b77919554e000f4
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-11-16 15:08:21 +0000

    Basic State pattern implementation

---
+++ Cave Divers/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform playerPos;
+
+    private IEnemyState currentState;
+    public PatrolState patrol = new PatrolState();
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        playerPos = FindObjectOfType<Player>().transform;
+        currentState = patrol;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.DoStateAction(this);
+    }
+}
---
+++ Cave Divers/Assets/Scripts/PatrolState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class PatrolState : IEnemyState
+{
+    Vector3 targetPoint;
+    float timer = 3f;
+    public IEnemyState DoStateAction(Enemy enemy)
+    {
+        if(timer <= 0)
+        {
+            PickNewPoint(enemy);
+            timer = 3f;
+        }
+        timer -= Time.deltaTime;
+        Debug.Log(targetPoint);
+        return enemy.patrol;
+    }
+
+    private void PickNewPoint(Enemy enemy)
+    {
+        float randomX = UnityEngine.Random.Range(-5f,5f);
+        float randomZ = UnityEngine.Random.Range(-5f, 5f);
+        targetPoint = enemy.transform.position + new Vector3(randomX, 0, randomZ);
+        enemy.agent.SetDestination(targetPoint);
+    }
+}
