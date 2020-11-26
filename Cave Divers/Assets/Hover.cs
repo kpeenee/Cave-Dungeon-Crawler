854dd0d5eb1dad9cfdd3016b28ad7909b3911120
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-11-26 16:29:08 +0000

    Weapon pickup UI

---
+++ Cave Divers/Assets/Hover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    [SerializeField] float yOffset = 1.75f;
+    Transform parent;
+
+    private void Start()
+    {
+        parent = transform.parent;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(parent.position.x, parent.position.y + yOffset, parent.position.z);
+    }
+}
