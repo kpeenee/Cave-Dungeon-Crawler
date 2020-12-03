eb7555131403c8fe00fec2a84e196bb8869c7775
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-12-03 13:41:40 +0000

    Health Bars Added To Goblin

---
+++ Cave Divers/Assets/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void UpdateHealth(float health)
+    {
+        slider.value = health;
+    }
+}
--- Cave Divers/Assets/Scripts/Health.cs
+++ Cave Divers/Assets/Scripts/Health.cs
@@ -7,14 +7,23 @@ public class Health : MonoBehaviour
 {
     [SerializeField] float maxHealth;
     private float currentHealth;
+    [SerializeField] HealthBar healthBar;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        if(healthBar != null)
+        {
+            healthBar.SetMaxHealth(maxHealth);
+        }
     }
     public void takeDamage(float damageAmount)
     {
         currentHealth -= damageAmount;
+        if(healthBar != null)
+        {
+            healthBar.UpdateHealth(currentHealth);
+        }
         Debug.Log("Ouch!! I have " + currentHealth + " health left!!");
         if(currentHealth<= 0)
         {
